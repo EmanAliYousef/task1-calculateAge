a40212aace5641f4a803603c6295a03d221804e0
Eman Ali <34413945+EmanAliYousef@users.noreply.github.com>
2021-08-30 01:24:41 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'task1calculateAge';
+  year=''
+  Age:Number=0
+  sendAge(){
+ const input=document.getElementById('input1') as HTMLInputElement
+ const value=input.value
+ this.year=value
+ this.Age=Number(2021-Number(this.year))
+ 
+  }
+
 }
